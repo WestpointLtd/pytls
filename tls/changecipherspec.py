2387303418f805ba3530ea7192a1e81eb8eb3d91
Richard J. Moore <rich@kde.org>
2015-06-20 14:41:56 +0100

    Complete change cipher spec

--- tls/changecipherspec.py
+++ tls/changecipherspec.py
@@ -6,11 +6,20 @@ class ChangeCipherSpecMessage(object):
     def __init__(self):
         self.bytes = ''
 
+    def value(self):
+        return ord(self.bytes[0])
+
     @classmethod
     def create(cls):
         self = cls()
         self.bytes = '\1'
 
+        return self
+
+    @classmethod
+    def from_bytes(cls, bytes):
+        self = cls()
+        self.bytes = bytes
         return self
 
     def __len__(self):
